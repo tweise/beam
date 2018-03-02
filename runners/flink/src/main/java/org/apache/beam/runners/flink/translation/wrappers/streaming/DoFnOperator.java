5e28848592b56f62581cf1c237e81cc6bcbf323d
Ben Sidhom <sidhom@google.com>
2018-03-01 20:30:23 -0800

    Revert "[BEAM-2140] Use ProcessFnRunner in DoFnRunner for executing SDF"
    This reverts commit 74ac703bbfa40d16e7d1115912768c7a63598d52.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
@@ -39,12 +39,10 @@ import javax.annotation.Nullable;
 import org.apache.beam.runners.core.DoFnRunner;
 import org.apache.beam.runners.core.DoFnRunners;
 import org.apache.beam.runners.core.NullSideInputReader;
-import org.apache.beam.runners.core.ProcessFnRunner;
 import org.apache.beam.runners.core.PushbackSideInputDoFnRunner;
 import org.apache.beam.runners.core.SideInputHandler;
 import org.apache.beam.runners.core.SideInputReader;
 import org.apache.beam.runners.core.SimplePushbackSideInputDoFnRunner;
-import org.apache.beam.runners.core.SplittableParDoViaKeyedWorkItems;
 import org.apache.beam.runners.core.StateInternals;
 import org.apache.beam.runners.core.StateNamespace;
 import org.apache.beam.runners.core.StateNamespaces;
@@ -354,13 +352,8 @@ public class DoFnOperator<InputT, OutputT>
             .scheduleAtFixedRate(
                 timestamp -> checkInvokeFinishBundleByTime(), bundleCheckPeriod, bundleCheckPeriod);
 
-    if (doFn instanceof SplittableParDoViaKeyedWorkItems.ProcessFn) {
-      pushbackDoFnRunner =
-          new ProcessFnRunner<>((DoFnRunner) doFnRunner, sideInputs, sideInputHandler);
-    } else {
-      pushbackDoFnRunner =
-          SimplePushbackSideInputDoFnRunner.create(doFnRunner, sideInputs, sideInputHandler);
-    }
+    pushbackDoFnRunner =
+        SimplePushbackSideInputDoFnRunner.create(doFnRunner, sideInputs, sideInputHandler);
   }
 
   @Override
