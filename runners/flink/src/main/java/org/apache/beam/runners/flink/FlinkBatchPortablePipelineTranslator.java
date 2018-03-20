36e34a33824982fbb848edf818a054152ee34261
Luke Cwik <lcwik@google.com>
2018-03-19 18:07:57 -0700

    Add stub context implementations.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkBatchPortablePipelineTranslator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkBatchPortablePipelineTranslator.java
@@ -1,5 +1,7 @@
 package org.apache.beam.runners.flink;
 
+import akka.dispatch.BatchingExecutor;
+import com.google.common.collect.Iterables;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
 import org.apache.beam.runners.core.construction.PTransformTranslation;
 import org.apache.beam.runners.core.construction.graph.PipelineNode;
@@ -13,6 +15,7 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PValue;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 
 import java.util.HashMap;
@@ -20,13 +23,58 @@ import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-public class FlinkBatchPortablePipelineTranslator implements FlinkPortablePipelineTranslator {
+public class FlinkBatchPortablePipelineTranslator implements FlinkPortablePipelineTranslator<FlinkBatchPortablePipelineTranslator.BatchTranslationContext> {
+
+  // Shared between batch and streaming
+  interface TranslationContext {
+    FlinkPipelineExecutionEnvironment getFlinkPipelineExecutionEnvironment();
+    <T> void addDataSet(String id, DataSet<T> dataSet);
+    <T> DataSet<T> getDataSetOrThrow(String id);
+  }
+
+  // For batch only
+  interface BatchTranslationContext extends TranslationContext {
+    ExecutionEnvironment getExecutionEnvironment();
+  }
+
+  private abstract class TranslationContextImpl {
+    private final Map<String, DataSet<?>> dataSets;
+    private final FlinkPipelineExecutionEnvironment env;
+
+    protected TranslationContextImpl(FlinkPipelineExecutionEnvironment env) {
+      this.env = env;
+      dataSets = new HashMap<>();
+    }
+
+    public <T> void addDataSet(String id, DataSet<T> dataSet) {
+      dataSets.put(id, dataSet);
+    }
+
+    public <T> DataSet<T> getDataSetOrThrow(String id) {
+      DataSet<T> dataSet = (DataSet<T>) dataSets.get(id);
+      if (dataSet == null) {
+        throw new IllegalArgumentException(
+            String.format("Unknown dataset for id %s.", id));
+      }
+      return dataSet;
+    }
+  }
+
+  private class BatchTranslationContextImpl extends TranslationContextImpl {
+    private final ExecutionEnvironment env;
+    private BatchTranslationContextImpl(
+        FlinkPipelineExecutionEnvironment flinkPipelineExecutionEnvironment,
+        ExecutionEnvironment environment) {
+      super(flinkPipelineExecutionEnvironment);
+      this.env = environment;
+    }
+  }
 
   interface PTransformTranslator<T> {
     void translate(String id, RunnerApi.Pipeline pipeline, T t);
   }
 
-  private final Map<String, PTransformTranslator<FlinkPipelineExecutionEnvironment>>
+  private final Map<String, PTransformTranslator<BatchTranslationContext>>
       urnToTransformTranslator = new HashMap<>();
 
   FlinkBatchPortablePipelineTranslator() {
@@ -35,23 +83,60 @@ public class FlinkBatchPortablePipelineTranslator implements FlinkPortablePipeli
 
   @Override
   public void translate(
-      FlinkPipelineExecutionEnvironment environment, RunnerApi.Pipeline pipeline) {
+      BatchTranslationContext context, RunnerApi.Pipeline pipeline) {
     QueryablePipeline p = QueryablePipeline.forPrimitivesIn(pipeline.getComponents());
     for (PipelineNode.PTransformNode transform : p.getTopologicallyOrderedTransforms()) {
       urnToTransformTranslator.getOrDefault(
           transform.getTransform().getSpec().getUrn(), this::urnNotFound)
-          .translate(transform.getId(), pipeline, environment);
+          .translate(transform.getId(), pipeline, context);
     }
   }
 
   public <T> void translateFlatten(
       String id,
       RunnerApi.Pipeline pipeline,
-      FlinkPipelineExecutionEnvironment env) {
+      BatchTranslationContext context) {
+    Map<String, String> allInputs = pipeline.getComponents().getTransformsOrThrow(id).getInputsMap();
+    DataSet<WindowedValue<T>> result = null;
 
+    if (allInputs.isEmpty()) {
+
+      // create an empty dummy source to satisfy downstream operations
+      // we cannot create an empty source in Flink, therefore we have to
+      // add the flatMap that simply never forwards the single element
+      DataSource<String> dummySource =
+          context.getExecutionEnvironment().fromElements("dummy");
+      result =
+          dummySource
+              .<WindowedValue<T>>flatMap(
+                  (s, collector) -> {
+                    // never return anything
+                  })
+              .returns(
+                  new CoderTypeInformation<>(
+                      WindowedValue.getFullCoder(
+                          (Coder<T>) VoidCoder.of(), GlobalWindow.Coder.INSTANCE)));
+    } else {
+      for (String pCollectionId : allInputs.values()) {
+        DataSet<WindowedValue<T>> current = context.getDataSetOrThrow(pCollectionId);
+        if (result == null) {
+          result = current;
+        } else {
+          result = result.union(current);
+        }
+      }
+    }
+
+    // insert a dummy filter, there seems to be a bug in Flink
+    // that produces duplicate elements after the union in some cases
+    // if we don't
+    result = result.filter(tWindowedValue -> true).name("UnionFixFilter");
+    context.addDataSet(Iterables.getOnlyElement(
+        pipeline.getComponents().getTransformsOrThrow(id).getOutputsMap().values()),
+        result);
   }
 
-  public <T> void urnNotFound(String id, RunnerApi.Pipeline pipeline, T t) {
+  public <T> void urnNotFound(String id, RunnerApi.Pipeline pipeline, BatchTranslationContext context) {
     throw new IllegalArgumentException(
         String.format("Unknown type of URN %s for PTrasnform with id %s.",
             pipeline.getComponents().getTransformsOrThrow(id).getSpec().getUrn(),
--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkPortablePipelineTranslator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkPortablePipelineTranslator.java
@@ -2,6 +2,6 @@ package org.apache.beam.runners.flink;
 
 import org.apache.beam.model.pipeline.v1.RunnerApi;
 
-public interface FlinkPortablePipelineTranslator {
-  void translate(FlinkPipelineExecutionEnvironment environment, RunnerApi.Pipeline pipeline);
+public interface FlinkPortablePipelineTranslator<T> {
+  void translate(T context, RunnerApi.Pipeline pipeline);
 }
